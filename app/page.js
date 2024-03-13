6cdaabe01d6dda9f298394321e990b6124dd08e8
nicktun <100433615+NickTun@users.noreply.github.com>
2024-03-13 23:09:37 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,12 +4,12 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Script
+      <Script
         strategy="lazyOnload"
         >{`
         document.umnicoWidgetHash = '20174e1ea499f17a0ac1c4b51b8a8fd3'; var x = document.createElement('script'); x.src = 'https://umnico.com/assets/widget-loader.js'; x.type = 'text/javascript'; x.charset = 'UTF-8'; x.async = true; document.body.appendChild(x);
         `}
-      </Script> */}
+      </Script>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
